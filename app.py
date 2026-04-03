dde057fc151565366559d30c4941115aea00ce27
Vijaypreet Singh Dhillon <101507250+Vijaypreetsingh@users.noreply.github.com>
2026-04-03 10:40:19 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -109,9 +109,9 @@ rfm = df.groupby('CustomerID').agg({
     'TotalPrice': 'sum'
 }).rename(columns={'InvoiceDate': 'Recency', 'InvoiceNo': 'Frequency', 'TotalPrice': 'Monetary'})
 
-rfm['R_Score'] = pd.qcut(rfm['Recency'], 4, labels=[4,3,2,1], duplicates='drop')
-rfm['F_Score'] = pd.qcut(rfm['Frequency'], 4, labels=[1,2,3,4], duplicates='drop')
-rfm['M_Score'] = pd.qcut(rfm['Monetary'], 4, labels=[1,2,3,4], duplicates='drop')
+rfm['R_Score'] = pd.qcut(rfm['Recency'], 4, labels=False, duplicates='drop')
+rfm['F_Score'] = pd.qcut(rfm['Frequency'], 4, labels=False, duplicates='drop')
+rfm['M_Score'] = pd.qcut(rfm['Monetary'], 4, labels=False, duplicates='drop')
 rfm['RFM_Score'] = rfm['R_Score'].astype(str) + rfm['F_Score'].astype(str) + rfm['M_Score'].astype(str)
 
 def segment(rfm_score):
